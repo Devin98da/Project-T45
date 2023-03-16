d791f688b13899aa5bbba5ae9aced6ffbcea9644
Madhuka Devinda <devin98da@gmail.com>
2023-03-16 16:14:44 +0530

    Add rotation and level design

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField] private Transform _playerTransform;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = _playerTransform.position + new Vector3(0,2f,-15f);
+    }
+}
--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     private Rigidbody _rBody;
+    [SerializeField] private float _thrustPower = 10f;
+    [SerializeField] private float _rotationPower = 20f;
+    [SerializeField] private AudioClip _thrustSound;
 
+    private AudioSource _audioSource;
 
     void Start()
     {
-        _rBody = GetComponent<Rigidbody>();    
+        _rBody = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -17,24 +23,64 @@ public class Rocket : MonoBehaviour
         ApplyThrust();
 
         ApplyRotation();
-    }
 
-    private static void ApplyRotation()
-    {
-        if (Input.GetKey(KeyCode.D))
-        {
-            
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-        }
     }
 
     private void ApplyThrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            _rBody.AddRelativeForce(Vector3.up);
+            _rBody.AddRelativeForce(Vector3.up * _thrustPower);
+            if (!_audioSource.isPlaying)
+            {
+                _audioSource.PlayOneShot(_thrustSound);
+            }
+        }
+        else
+        {
+            _audioSource.Stop();
+        }
+    } // Apply thrust
+
+    private void ApplyRotation()
+    {
+        _rBody.freezeRotation = true;
+
+        float rotation = _rotationPower * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotation);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotation);
+
+        }
+        _rBody.freezeRotation = false;
+
+    } // Apply rotation
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Freindly":
+                LoadNextLevel();
+                break;
+            case "Obstacle":
+                SceneManager.LoadScene(0);
+                break;
+            default:
+                break;
         }
     }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+
 } // class
